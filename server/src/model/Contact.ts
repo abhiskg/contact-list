438a4fde7f040c5f02d6f5fc26fe1e6e43388d7a
Abhishek Sen Gupta <abhiskg20@gmail.com>
2023-04-04 11:55:09 +0600

    Contact model created

---
+++ server/src/model/Contact.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from "mongoose";
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is Required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is Required"],
+      unique: true,
+    },
+    phoneNo: {
+      type: Number,
+      required: [true, "Phone No is Required"],
+    },
+    address: {
+      type: String,
+      required: [true, "Address is Required"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("Contact", contactSchema);
